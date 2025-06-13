e5d524dc2df6291f15b48f863e43721832b280ea
Muharilmulyadi/alprog-e <arilmulyadi2@gmail.com>
2025-06-13 10:07:03 +0800

    Add files via upload

---
+++ meet8.py
@@ -0,0 +1,10 @@
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+ 
+# a. menampilkan data terakhir
+print("data terakhir:", number[-1])
+
+# b. menampilkan data pertama
+print("data pertama:", number[0])
+
+# c. menampilkan data ke-3 sampai ke-6 (undeks 2 hingga 5)
+print("data ke-3 sampai ke-6:", number[2:6])
